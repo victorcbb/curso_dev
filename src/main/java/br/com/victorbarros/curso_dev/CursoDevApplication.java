d31741e7f7ecd1510b3d6ca503300dcf966a1033
Victor <victorclovisb@hotmail.com>
2024-04-16 20:41:15 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/victorbarros/curso_dev/CursoDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorbarros.curso_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victorbarros/curso_dev/CursoDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorbarros.curso_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
