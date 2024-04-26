357e79d97bf3feb330ff453c6b1e55e13ead042d
Victor <victorclovisb@hotmail.com>
2024-04-25 21:25:46 -0300

    feat: Lançando exceção

--- src/main/java/br/com/victorbarros/curso_dev/modules/courses/useCases/CreateCourseUseCase.java
+++ src/main/java/br/com/victorbarros/curso_dev/modules/courses/useCases/CreateCourseUseCase.java
@@ -3,6 +3,7 @@ package br.com.victorbarros.curso_dev.modules.courses.useCases;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.victorbarros.curso_dev.exceptions.CourseNameFoundException;
 import br.com.victorbarros.curso_dev.modules.courses.CourseEntity;
 import br.com.victorbarros.curso_dev.modules.courses.CourseRepository;
 
@@ -13,6 +14,11 @@ public class CreateCourseUseCase {
   private CourseRepository courseRepository;
 
   public CourseEntity execute(CourseEntity courseEntity) {
+    this.courseRepository.findByName(courseEntity.getName())
+        .ifPresent((name) -> {
+          throw new CourseNameFoundException();
+        });
+
     return this.courseRepository.save(courseEntity);
   }
 }
