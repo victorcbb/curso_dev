a6ae8cbde8065b26ef31ab05158aff816930e6bc
Victor <victorclovisb@hotmail.com>
2024-04-29 20:49:11 -0300

    feat: Remove segurança

---
+++ src/main/java/br/com/victorbarros/curso_dev/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package br.com.victorbarros.curso_dev.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable());
+
+    return http.build();
+  }
+}
