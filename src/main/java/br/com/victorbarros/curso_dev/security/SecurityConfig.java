bbe1308f8decc68ee2a8e6b1d314e1519eebd0f1
Victor <victorclovisb@hotmail.com>
2024-04-29 21:20:40 -0300

    feat: Permite acesso apena a rota de criação

--- src/main/java/br/com/victorbarros/curso_dev/security/SecurityConfig.java
+++ src/main/java/br/com/victorbarros/curso_dev/security/SecurityConfig.java
@@ -12,7 +12,11 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable());
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/course/create").permitAll();
+          auth.anyRequest().authenticated();
+        });
 
     return http.build();
   }
