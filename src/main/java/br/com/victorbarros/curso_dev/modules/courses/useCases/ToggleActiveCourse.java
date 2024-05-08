00698084571431a71fcc663a6e0b126c33f5742d
Victor <victorclovisb@hotmail.com>
2024-05-07 21:42:55 -0300

    feat: Caso de uso que altera entre curso ativo e não ativo

---
+++ src/main/java/br/com/victorbarros/curso_dev/modules/courses/useCases/ToggleActiveCourse.java
@@ -0,0 +1,36 @@
+package br.com.victorbarros.curso_dev.modules.courses.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.victorbarros.curso_dev.exceptions.CourseNotFoundException;
+import br.com.victorbarros.curso_dev.modules.courses.CourseEntity;
+import br.com.victorbarros.curso_dev.modules.courses.CourseRepository;
+import br.com.victorbarros.curso_dev.types.Active;
+
+@Service
+public class ToggleActiveCourse {
+
+  @Autowired
+  private CourseRepository courseRepository;
+
+  public CourseEntity execute(UUID id) {
+    var course = this.courseRepository.findById(id)
+        .orElseThrow(() -> {
+          throw new CourseNotFoundException();
+        });
+
+    if (course.getActive().equals(Active.NO)) {
+      course.setActive(Active.YES);
+    }
+    if (course.getActive().equals(Active.YES)) {
+      course.setActive(Active.NO);
+    }
+
+    this.courseRepository.save(course);
+
+    return course;
+  }
+}
